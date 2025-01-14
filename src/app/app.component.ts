aecab0adc5e9c5fad76012149b7426e6775cc3a6
Marcysiox <marcinchowaniec21@gmail.com>
2025-01-14 17:46:16 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,10 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Tennis AI Chat';
-  userMessage: string = '';
-  chatHistory: { sender: 'user' | 'ai'; message: string }[] = [];
-
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  sendMessage(): void {
-    if (this.userMessage.trim()) {
-      this.chatHistory.push({ sender: 'user', message: this.userMessage });
-      this.generateAIResponse(this.userMessage);
-      this.userMessage = '';
-    }
-  }
-
-  generateAIResponse(userInput: string): void {
-    // Symulowana odpowiedź AI
-    const aiResponse = `AI: I received your message "${userInput}". Let's play some tennis!`;
-    setTimeout(() => {
-      this.chatHistory.push({ sender: 'ai', message: aiResponse });
-    }, 1000); // Dodanie opóźnienia dla realizmu
-  }
-}
+export class AppComponent {}
